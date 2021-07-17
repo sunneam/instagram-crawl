10cca1df94d414d188befdec88d3272335d2256b
fulun <poo4318@dingtalk.com>
2021-07-17 13:20:57 +0800

    测试jenkins自动部署

--- model/mongo.go
+++ model/mongo.go
@@ -10,10 +10,12 @@ var(
 	url =""
 )
 func Init()  {
+
 	session,err:=mgo.Dial(url)
 	if err!=nil {
 		panic(err)
 	}
+	fmt.Println("测试")
 	session.SetMode(mgo.Monotonic, true)
 	Db=session.DB("veryins")
 	fmt.Println("连接数据库成功")
