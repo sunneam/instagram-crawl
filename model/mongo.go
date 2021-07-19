2f703b12377c2c30ebf85eb91cdfe8b2d7e3259e
fulun <poo4318@dingtalk.com>
2021-07-19 22:16:58 +0800

    1

--- model/mongo.go
+++ model/mongo.go
@@ -16,5 +16,8 @@ func Init()  {
 		panic(err)
 	}
 	session.SetMode(mgo.Monotonic, true)
+	session.SetMode(mgo.Monotonic, true)
+	session.SetMode(mgo.Monotonic, true)
+	session.SetMode(mgo.Monotonic, true)
 	Db=session.DB("veryins")
 }
